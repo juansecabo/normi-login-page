40d71d5da3a323fc98822aee01edddc16f83b760
juansecabo <juansecabo@users.noreply.github.com>
2026-05-14 16:59:55 -0500

    estudiante: tile y página Mis Consultas (re-aplicado tras revert #2)

--- src/App.tsx
+++ src/App.tsx
@@ -78,6 +78,7 @@ import Consultas from "./pages/Consultas";
 import ConsultaDetalle from "./pages/ConsultaDetalle";
 import ConsultaPublica from "./pages/ConsultaPublica";
 import MisConsultas from "./pages/padre/MisConsultas";
+import MisConsultasEstudiante from "./pages/estudiante/MisConsultasEstudiante";
 import RegistrosComportamiento from "./pages/RegistrosComportamiento";
 import RestringirNormy from "./pages/rector/RestringirNormy";
 import TablaNotasRouter from "./pages/TablaNotasRouter";
@@ -173,6 +174,7 @@ const App = () => (
           <Route path="/consultas/:id" element={<ConsultaDetalle />} />
           <Route path="/consulta/:id" element={<ConsultaPublica />} />
           <Route path="/padre/consultas" element={<MisConsultas />} />
+          <Route path="/estudiante/consultas" element={<MisConsultasEstudiante />} />
 
           {/* Registros de Comportamiento */}
           <Route path="/registros-comportamiento" element={<RegistrosComportamiento />} />
