5dfb73da45ed8a42cfbf1697ea1865824fa1015d
juansecabo <juansecabo@users.noreply.github.com>
2026-05-25 15:28:07 -0500

    consulta editar: dialog con scroll vertical (max-h-90vh)

--- src/pages/ConsultaDetalle.tsx
+++ src/pages/ConsultaDetalle.tsx
@@ -1491,7 +1491,7 @@ export default function ConsultaDetalle() {
       </Dialog>
 
       <Dialog open={editarOpen} onOpenChange={setEditarOpen}>
-        <DialogContent className="max-w-2xl">
+        <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto">
           <DialogHeader>
             <DialogTitle>Editar consulta</DialogTitle>
           </DialogHeader>
